fee286c0ce06be2c350133b9e8704386be657f7e
Audrey Long <aulong@microsoft.com>
2022-09-13 12:18:47 -0700

    got sas components working

--- Client/ReadDeploymentStatus.cs
+++ Client/ReadDeploymentStatus.cs
@@ -3,6 +3,7 @@ using Azure.Identity;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Azure.Messaging.ServiceBus.Administration;
 
 namespace ProviderFunctions
 {
@@ -12,40 +13,18 @@ namespace ProviderFunctions
         [FunctionName("ReadDeploymentStatus")]
 
         public void Run([ServiceBusTrigger("%CommandQueueName%", 
-            Connection = "ServiceBusConnection")]string command, ILogger log)
+            Connection = "CommandQueueSaSConnectionStringPk")]string command, ILogger log)
         {
-            //var customerTenantId = Environment.GetEnvironmentVariable("CustomerTenantId");
-            //var providerClientId = Environment.GetEnvironmentVariable("ClientId");
-            //var providerClientSecret = Environment.GetEnvironmentVariable("ClientSecret");
-           // var credential = new ClientSecretCredential(customerTenantId, providerClientId, providerClientSecret);
-           // var serviceBusNamespace = Environment.GetEnvironmentVariable("ServiceBusNamespace");
-          
             var responseQueue = Environment.GetEnvironmentVariable("ReponseQueueName");
-            var sasPrimaryKeyConnectionString  = Environment.GetEnvironmentVariable("CommandQueueSaSConnectionStringPk");
-            var client = new ServiceBusClient(sasPrimaryKeyConnectionString);
+            var responseQueueConnectionString  = Environment.GetEnvironmentVariable("ResponseQueueSaSConnectionStringPk");
+            var client = new ServiceBusClient(responseQueueConnectionString);
 
             log.LogInformation($"ReadDeploymentStatus triggered with command: {command}");
 
-           // var sender = client.CreateSender(responseQueue);
-           // var message = new ServiceBusMessage(command);
-           // sender.SendMessageAsync(message).GetAwaiter().GetResult();
+            var sender = client.CreateSender(responseQueue);
+            var message = new ServiceBusMessage(command);
+            sender.SendMessageAsync(message).GetAwaiter().GetResult();
 
-           /* 
-            var receiver = client.CreateReceiver(deploymentStatusQueueName);
-
-            var receivedMessage = receiver.ReceiveMessageAsync().GetAwaiter().GetResult();
-            if (receivedMessage == null)
-            {   
-                log.LogInformation($">> No message read -- {DateTime.Now}");
-                return;
-            }
-            
-            var body = receivedMessage.Body.ToString();
-            log.LogInformation($">> MESSAGE RECEIVED: {body} -- {DateTime.Now}");
-
-            // Mark the message for deletion on the queue
-            receiver.CompleteMessageAsync(receivedMessage).GetAwaiter().GetResult();
-        */
         }
     }
 }
