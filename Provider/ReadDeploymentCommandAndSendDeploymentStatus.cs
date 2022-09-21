2127434b9a87565079405f4ddcd13200060b83b8
Brian Davis <briandavis@microsoft.com>
2022-09-21 12:30:15 -0400

    removed unused imports

--- Provider/ReadDeploymentCommandAndSendDeploymentStatus.cs
+++ Provider/ReadDeploymentCommandAndSendDeploymentStatus.cs
@@ -1,52 +1,49 @@
 using System;
-using System.Threading.Tasks;
 using Azure.Identity;
 using Azure.Messaging.ServiceBus;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Wtw.SecretRotationPOC.ServiceBus;
-namespace CustomerFunctions
+namespace CustomerFunctions;
+
+// Pre-function work
+// 1) create a managed identity in the Customer subscription to interface with the service bus
+// 2) wire the managed identity in the IAM service bus blade and give it the "Service Bus Data Owner" permissions
+public class ReadDeploymentCommandAndSendDeploymentStatus
 {
-    // Pre-function work
-    // 1) create a managed identity in the Customer subscription to interface with the service bus
-    // 2) wire the managed identity in the IAM service bus blade and give it the "Service Bus Data Owner" permissions
-    public class ReadDeploymentCommandAndSendDeploymentStatus
+    // This customer-side service bus trigger connects to the customer's
+    // service bus with a managed identity to read the contents.
+    [FunctionName("ReadDeploymentCommandAndSendDeploymentStatus")]
+
+    // ServiceBusConnection resolves to ServiceBusConnection__fullyQualifiedNamespace
+    // which is referenced within the local.settings.json file to use the managed identity
+    public static void Run([ServiceBusTrigger("%InitiatedDeploymentQueueName%",
+        Connection = "ServiceBusConnection")]string command, ILogger log)
     {
-        // This customer-side service bus trigger connects to the customer's
-        // service bus with a managed identity to read the contents.
-        [FunctionName("ReadDeploymentCommandAndSendDeploymentStatus")]
+        log.LogInformation($"ReadDeploymentCommandAndSendDeploymentStatus triggered with command: {command}");
+        ServiceBusService sbService = new(log);
+        DateTime now = DateTime.Now;
+        TimeSpan span = now.AddYears(1) - now;
 
-        // ServiceBusConnection resolves to ServiceBusConnection__fullyQualifiedNamespace
-        // which is referenced within the local.settings.json file to use the managed identity
-        public static void Run([ServiceBusTrigger("%InitiatedDeploymentQueueName%",
-            Connection = "ServiceBusConnection")]string command, ILogger log)
-        {
-            log.LogInformation($"ReadDeploymentCommandAndSendDeploymentStatus triggered with command: {command}");
-            ServiceBusService sbService = new(log);
-            DateTime now = DateTime.Now;
-            TimeSpan span = now.AddYears(1) - now;
-            
-            // retrieve connection strings from Srv Bus for queues
-            var rootString = Environment.GetEnvironmentVariable("RootManageSharedAccessKeyConnectionString");
-            var commandQName = Environment.GetEnvironmentVariable("CommandQueueName");
-            var responseQName = Environment.GetEnvironmentVariable("ResponseQueueName");
-            var cmdSaPolicyName = Environment.GetEnvironmentVariable("CommandQueueAuthPolName");
-            var respSaPolicyName = Environment.GetEnvironmentVariable("ResponseQueueAuthPolName");
+        // retrieve connection strings from Srv Bus for queues
+        string rootString = Environment.GetEnvironmentVariable("RootManageSharedAccessKeyConnectionString");
+        string commandQName = Environment.GetEnvironmentVariable("CommandQueueName");
+        string responseQName = Environment.GetEnvironmentVariable("ResponseQueueName");
+        string cmdSaPolicyName = Environment.GetEnvironmentVariable("CommandQueueAuthPolName");
+        string respSaPolicyName = Environment.GetEnvironmentVariable("ResponseQueueAuthPolName");
 
-            var cmdConnectionStr =  sbService.GetSASTokenConnectionString(rootString, commandQName, cmdSaPolicyName, span);
-            var respConnectionStr =  sbService.GetSASTokenConnectionString(rootString, responseQName, respSaPolicyName, span);
-            var credential = new DefaultAzureCredential();
+        var cmdConnectionStr =  sbService.GetSASTokenConnectionString(rootString, commandQName, cmdSaPolicyName, span);
+        var respConnectionStr =  sbService.GetSASTokenConnectionString(rootString, responseQName, respSaPolicyName, span);
+        var credential = new DefaultAzureCredential();
 
-            var serviceBusNamespace = Environment.GetEnvironmentVariable("ServiceBusConnection__fullyQualifiedNamespace");
-            var commandQueue = Environment.GetEnvironmentVariable("CommandQueueName");
-            var client = new ServiceBusClient(serviceBusNamespace, credential);
-            string encryptedCommandMessage = command; // TODO encrypt the connection strings and what not
+        string serviceBusNamespace = Environment.GetEnvironmentVariable("ServiceBusConnection__fullyQualifiedNamespace");
+        string commandQueue = Environment.GetEnvironmentVariable("CommandQueueName");
+        var client = new ServiceBusClient(serviceBusNamespace, credential);
+        string encryptedCommandMessage = command; // TODO encrypt the connection strings and what not
 
-            var sender = client.CreateSender(commandQueue);
-            var message = new ServiceBusMessage(encryptedCommandMessage);
-            sender.SendMessageAsync(message).GetAwaiter().GetResult();
-            
-        }
+        var sender = client.CreateSender(commandQueue);
+        var message = new ServiceBusMessage(encryptedCommandMessage);
+        sender.SendMessageAsync(message).GetAwaiter().GetResult();
+
     }
 }
