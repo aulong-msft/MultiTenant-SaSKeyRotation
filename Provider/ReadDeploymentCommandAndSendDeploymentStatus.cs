b6348f0f726622e88849a32a06360783c8c099b2
Brian Davis <briandavis@microsoft.com>
2022-09-21 20:00:47 -0400

    added code to encrypt the sent message

--- Provider/ReadDeploymentCommandAndSendDeploymentStatus.cs
+++ Provider/ReadDeploymentCommandAndSendDeploymentStatus.cs
@@ -4,6 +4,11 @@ using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Wtw.SecretRotationPOC.ServiceBus;
+using Azure.Security.KeyVault.Certificates;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace CustomerFunctions;
 
 // Pre-function work
@@ -11,6 +16,25 @@ namespace CustomerFunctions;
 // 2) wire the managed identity in the IAM service bus blade and give it the "Service Bus Data Owner" permissions
 public class ReadDeploymentCommandAndSendDeploymentStatus
 {
+
+
+    public static string Encrypt(string plainText, X509Certificate2 cert)
+    {
+        RSACng publicKey = (RSACng)cert.GetRSAPublicKey();
+        byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+        byte[] encryptedBytes = publicKey.Encrypt(plainBytes, RSAEncryptionPadding.Pkcs1);
+        string encryptedText = Convert.ToBase64String(encryptedBytes);
+        return encryptedText;
+    }
+
+    public static string Decrypt(string encryptedText, X509Certificate2 cert)
+{
+    RSACng privateKey = (RSACng)cert.GetRSAPrivateKey();
+    byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+    byte[] decryptedBytes = privateKey.Decrypt(encryptedBytes, RSAEncryptionPadding.Pkcs1);
+    string decryptedText = Encoding.UTF8.GetString(decryptedBytes);
+    return decryptedText;
+}
     // This customer-side service bus trigger connects to the customer's
     // service bus with a managed identity to read the contents.
     [FunctionName("ReadDeploymentCommandAndSendDeploymentStatus")]
@@ -31,17 +55,27 @@ public class ReadDeploymentCommandAndSendDeploymentStatus
         string responseQName = Environment.GetEnvironmentVariable("ResponseQueueName");
         string cmdSaPolicyName = Environment.GetEnvironmentVariable("CommandQueueAuthPolName");
         string respSaPolicyName = Environment.GetEnvironmentVariable("ResponseQueueAuthPolName");
+        string vaultUrl = Environment.GetEnvironmentVariable("KeyVaultURL");
 
-        var cmdConnectionStr =  sbService.GetSASTokenConnectionString(rootString, commandQName, cmdSaPolicyName, span);
-        var respConnectionStr =  sbService.GetSASTokenConnectionString(rootString, responseQName, respSaPolicyName, span);
+        var cmdConnectionStr = sbService.GetSASTokenConnectionString(rootString, commandQName, cmdSaPolicyName, span);
+        var respConnectionStr = sbService.GetSASTokenConnectionString(rootString, responseQName, respSaPolicyName, span);
         var credential = new DefaultAzureCredential();
 
         string serviceBusNamespace = Environment.GetEnvironmentVariable("ServiceBusConnection__fullyQualifiedNamespace");
         string commandQueue = Environment.GetEnvironmentVariable("CommandQueueName");
-        var client = new ServiceBusClient(serviceBusNamespace, credential);
-        string encryptedCommandMessage = command; // TODO encrypt the connection strings and what not
+        var sbClient = new ServiceBusClient(serviceBusNamespace, credential);
+        
+        // Create a new certificate client using the default credential from Azure.Identity 
+        var certClient =
+        new CertificateClient(vaultUri: new Uri(vaultUrl), credential: new DefaultAzureCredential());
+        KeyVaultCertificateWithPolicy certificateWithPolicy = certClient.GetCertificate("clientpoc");
+        KeyVaultCertificate certificate =
+        certClient.GetCertificateVersion(certificateWithPolicy.Name, certificateWithPolicy.Properties.Version);
+        X509Certificate2 x509cer = new X509Certificate2(certificate.Cer);
 
-        var sender = client.CreateSender(commandQueue);
+        string encryptedCommandMessage = Encrypt(command, x509cer);
+
+        var sender = sbClient.CreateSender(commandQueue);
         var message = new ServiceBusMessage(encryptedCommandMessage);
         sender.SendMessageAsync(message).GetAwaiter().GetResult();
 
