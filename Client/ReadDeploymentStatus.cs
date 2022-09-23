805a47302bbc4222ad8e146cd7656d7cc037575c
Audrey Long <aulong@microsoft.com>
2022-09-23 08:57:09 -0700

    wired up the solution with rotation

--- Client/ReadDeploymentStatus.cs
+++ Client/ReadDeploymentStatus.cs
@@ -4,117 +4,107 @@ using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-using Azure.Security.KeyVault.Certificates;
-using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
 using System.Text;
 using Azure.Security.KeyVault.Secrets;
-
+using System.IO;
 
 namespace ClientFunctions
 {
     public class ReadDeploymentStatus
     {
-        public static string Encrypt(string plainText, X509Certificate2 cert)
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
         {
-            try
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
             {
-                RSACng publicKey = (RSACng)cert.GetRSAPublicKey();
-                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
-                byte[] encryptedBytes = publicKey.Encrypt(plainBytes, RSAEncryptionPadding.Pkcs1);
-                string encryptedText = Convert.ToBase64String(encryptedBytes);
-                return encryptedText;
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Couldn't encrypt message.");
-                return "";
-            }
+
+            return plaintext;
         }
 
-        public static string Decrypt(string encryptedText, X509Certificate2 cert)
-        {
 
-            try
-            {
-                RSACng privateKey = (RSACng)cert.GetRSAPrivateKey();
-                byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
-                byte[] decryptedBytes = privateKey.Decrypt(encryptedBytes, RSAEncryptionPadding.Pkcs1);
-                string decryptedText = Encoding.UTF8.GetString(decryptedBytes);
-                return decryptedText;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Couldn't decrypt message.");
-                return "";
-            }
-
-        }
         // A function for the CLIENT to try reading from the Customer's deployment status
         [FunctionName("ReadDeploymentStatus")]
         public void Run([ServiceBusTrigger("%CommandQueueName%",
-            Connection = "CommandQueueSaSConnectionStringPk")]string command, ILogger log)
+            Connection = "CommandQueueSaSConnectionStringPk")]byte[] command, ILogger log)
         {
 
             Console.WriteLine($"received {command} as the message");
             var responseQueue = Environment.GetEnvironmentVariable("ReponseQueueName");
             var responseQueueConnectionString = Environment.GetEnvironmentVariable("ResponseQueueSaSConnectionStringPk");
             var keyVaultUrl = Environment.GetEnvironmentVariable("KeyVaultName");
-
+            string AESKey = Environment.GetEnvironmentVariable("AESKey");
+            string IV = Environment.GetEnvironmentVariable("IV");
             string vaultUrl = Environment.GetEnvironmentVariable("KeyVaultURL");
 
+            string decryptedCommandMessage;
+            byte[] aeskey = Encoding.ASCII.GetBytes(AESKey);
+            byte[] iv = Encoding.ASCII.GetBytes(IV);
 
-            // Create a new certificate client using the default credential from Azure.Identity 
-            var certClient =
-            new CertificateClient(vaultUri: new Uri(vaultUrl), credential: new DefaultAzureCredential());
-            KeyVaultCertificateWithPolicy certificateWithPolicy = certClient.GetCertificate("clientpoc2");
-            KeyVaultCertificate certificate =
-            certClient.GetCertificateVersion(certificateWithPolicy.Name, certificateWithPolicy.Properties.Version);
-            X509Certificate2 x509cer = new X509Certificate2(certificate.Cer);
-
-            string decryptedMsg = Decrypt(command, x509cer);
-            if (!decryptedMsg.Equals(""))
-                Console.WriteLine($"Decrypted Msg: {decryptedMsg}");
-            else
+            using (Aes myAes = Aes.Create())
             {
-                Console.WriteLine("Problem Decrypting Msg");
-                return;
+                decryptedCommandMessage = DecryptStringFromBytes_Aes(command, aeskey, iv);
             }
+                Console.WriteLine(decryptedCommandMessage);
 
             //KV code 
-            //0.) Investigate function restarting
-            //1.) Create system assigned managed identity to access the KV with AzureDefaultCredential
-            //   var clientKV = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-
-            //2.) Decrypt the message received from the command queue
-            //3.) Store the new values in the KV
+            // Create system assigned managed identity to access the KV with AzureDefaultCredential
+            var clientKV = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
 
             // Retrieve secrets using the secret client
-            // KeyVaultSecret CommandQueuePK = clientKV.GetSecret("CommandQueuePK");
-            //  KeyVaultSecret ReponseQueuePK = clientKV.GetSecret("ReponseQueuePK");
-            // KeyVaultSecret Encryptionkey = clientKV.GetSecret("ClientEncryptionKey");
+            KeyVaultSecret CommandQSasString = clientKV.GetSecret("CommandQSasString");
+            KeyVaultSecret ReponseQueuePK = clientKV.GetSecret("ReponseQueuePK");
 
-            //            Console.WriteLine(ReponseQueuePK.Name);
-            //           Console.WriteLine(ReponseQueuePK.Value);
-            //  Console.WriteLine(Encryptionkey.Value);
-
-
+            Console.WriteLine(CommandQSasString.Value);
             // Update Secrets using thr secret client
-            /* var secretNewValue1 = new KeyVaultSecret("CommandQueuePK", "bhjd4DDgsa");
-             secretNewValue1.Properties.ExpiresOn = DateTimeOffset.Now.AddYears(1);
-             clientKV.SetSecret(secretNewValue1);
-
-             var secretNewValue2 = new KeyVaultSecret("ReponseQueuePK", "bhjd4DDgsa");
-             secretNewValue2.Properties.ExpiresOn = DateTimeOffset.Now.AddYears(1);
-             clientKV.SetSecret(secretNewValue2);
-             */
+            var secretNewValue1 = new KeyVaultSecret("CommandQSasString", decryptedCommandMessage);
+            //secretNewValue1.Properties.ExpiresOn = DateTimeOffset.Now.AddYears(1);
+            clientKV.SetSecret(secretNewValue1);
+            
+            Console.WriteLine(secretNewValue1.Value);
+            
             //4.) Send an ACK message back to the response queuue
-            // var client = new ServiceBusClient(ReponseQueuePK.Value);
-            //  log.LogInformation($"ReadDeploymentStatus triggered with command: {command}");
+            var client = new ServiceBusClient(ReponseQueuePK.Value);
+            log.LogInformation($"ReadDeploymentStatus triggered with command: {command}");
 
-            // var sender = client.CreateSender(responseQueue);
-            // var message = new ServiceBusMessage(command);
-            //  sender.SendMessageAsync(message).GetAwaiter().GetResult();
+            var sender = client.CreateSender(responseQueue);
+            var message = new ServiceBusMessage(command);
+            sender.SendMessageAsync(message).GetAwaiter().GetResult();
 
         }
     }
--- Provider/ReadDeploymentCommandAndSendDeploymentStatus.cs
+++ Provider/ReadDeploymentCommandAndSendDeploymentStatus.cs
@@ -4,8 +4,6 @@ using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Wtw.SecretRotationPOC.ServiceBus;
-using Azure.Security.KeyVault.Certificates;
-using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
 using System.Text;
 using System.IO;
@@ -17,45 +15,6 @@ namespace CustomerFunctions;
 // 2) wire the managed identity in the IAM service bus blade and give it the "Service Bus Data Owner" permissions
 public class ReadDeploymentCommandAndSendDeploymentStatus
 {
-
-
-    public static string Encrypt(string plainText, X509Certificate2 cert)
-    {
-        try
-        {
-            RSACng publicKey = (RSACng)cert.GetRSAPublicKey();
-            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
-            byte[] encryptedBytes = publicKey.Encrypt(plainBytes, RSAEncryptionPadding.);
-            string encryptedText = Convert.ToBase64String(encryptedBytes);
-            return encryptedText;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Couldn't encrypt message.");
-            return "";
-        }
-    }
-
-    public static string Decrypt(string encryptedText, X509Certificate2 cert)
-    {
-
-        try
-        {
-            RSACng privateKey = (RSACng)cert.GetRSAPrivateKey();
-            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
-            byte[] decryptedBytes = privateKey.Decrypt(encryptedBytes, RSAEncryptionPadding.Pkcs1);
-            string decryptedText = Encoding.UTF8.GetString(decryptedBytes);
-            return decryptedText;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Couldn't decrypt message.");
-            return "";
-        }
-
-    }
-
-
     static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
     {
         // Check arguments.
@@ -119,7 +78,7 @@ public class ReadDeploymentCommandAndSendDeploymentStatus
         string respSaPolicyName = Environment.GetEnvironmentVariable("ResponseQueueAuthPolName");
         string vaultUrl = Environment.GetEnvironmentVariable("KeyVaultURL");
         string AESKey = Environment.GetEnvironmentVariable("AESKey");
-
+        string IV = Environment.GetEnvironmentVariable("IV");
         var cmdConnectionStr = sbService.GetSASTokenConnectionString(rootString, commandQName, cmdSaPolicyName, span);
         var respConnectionStr = sbService.GetSASTokenConnectionString(rootString, responseQName, respSaPolicyName, span);
         var credential = new DefaultAzureCredential();
@@ -137,12 +96,13 @@ public class ReadDeploymentCommandAndSendDeploymentStatus
         X509Certificate2 x509cer = new X509Certificate2(certificate.Cer);*/
 
         byte[] encryptedCommandMessage;
-        byte[] aeskey = Convert.FromBase64String(AESKey);
-        
+        byte[] aeskey = Encoding.ASCII.GetBytes(AESKey);
+        byte[] iv = Encoding.ASCII.GetBytes(IV);
         using (Aes myAes = Aes.Create())
         {
-            encryptedCommandMessage = EncryptStringToBytes_Aes(cmdConnectionStr.Result, aeskey, myAes.IV);
+            encryptedCommandMessage = EncryptStringToBytes_Aes(cmdConnectionStr.Result, aeskey, iv);
         }
+        
         var sender = sbClient.CreateSender(commandQueue);
         var message = new ServiceBusMessage(new BinaryData(encryptedCommandMessage));
         sender.SendMessageAsync(message).GetAwaiter().GetResult();
